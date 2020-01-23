c05bc5a99533a3c96d3e10e856bf86a4660c9a09
sangelici <sangelici94@gmail.com>
2020-01-22 20:05:56 -0500

    Completed fizzbuzz problem
    - reset fizzbuzz and maxChar for future practice

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,39 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+}
 
 module.exports = fizzBuzz;
+
+
+// ATTEMPT
+// I had it right, but i couldn't pass the tests for whatever reason.
+// One thing I could improve is the order of conditions in the if statement
+// function fizzBuzz(n) {
+//     for ( let i = 1; i < n; i++ ) {
+//         if ( i % 3 === 0) {
+//             console.log('fizz')
+//         } else if ( i % 5 === 0 ) {
+//             console.log('buzz')
+//         } else if ( i % 3 === 0 && i % 5 === 0 ) {
+//             console.log('fizzbuzz')
+//         }
+//         return(i)
+//     }
+// }
+
+// SOLUTION
+// function fizzBuzz(n) {
+//     for ( let i = 1; i <= n; i++ ) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzbuzz');
+//         } else if (i % 3 === 0) {
+//             console.log('fizz');
+//         } else if (i % 5 === 0) {
+//             console.log('buzz');
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
