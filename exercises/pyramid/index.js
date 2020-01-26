1f3669318390830b6c170f083abf7a4a93f301dd
sangelici <sangelici94@gmail.com>
2020-01-26 17:23:06 -0500

    Complete pyramid problem
    - loop and recursive solutions
    - very VERY hard, did not complete on my own, got half way through

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,61 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    // base case
+    if (n === row) {
+        return;
+    }
+
+    // if # of columns = the length of the string
+    if (level.length === 2 * n - 1) {
+        console.log(level)
+        // loop again
+        return pyramid(n, row + 1);
+    }
+
+    // initializes midpoint
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add;
+    
+    // if the string hasn't been finished
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = "#";
+    } else {
+        add = " ";
+    }
+
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+// SOLUTION ONE:
+// function pyramid(n) {
+
+//     // set midpoint of string (only need to define once)
+//     const midpoint = Math.floor((2 * n - 1) / 2)
+
+//     // loop through the # of rows ( total rows = n )
+//     for (let row = 0; row < n; row++) {
+//         // initialize an empty string
+//         let level = '';
+
+//         // loop through the columns (pattern is 3, 5, 7, 9, etc...)
+//         for (let col = 0; col < 2 * n - 1; col++) {
+//             // if the index[-1] is less than the col
+//             // OR
+//             // if the index[+1] is more than the col
+//             if (midpoint - row <= col && midpoint + row >= col) {
+//                 level += '#';
+//             } else {
+//                 level += ' '
+//             }
+//         }
+
+//         console.log(level)
+//     }
+
+// }
+
+// SOLUTION TWO: RECURSION
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,98 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// default row to start on first row
+// default stair as an empty string
+function steps(n, row = 0, stair = '') {
+    // Base Case
+    // if (row === end), we've hit the end of our problem
+    if (n === row) {
+        return;
+    }
+
+    // if the 'stair' string has a length === n, then we are at the end of a row
+    if (n === stair.length) {
+        console.log(stair);
+        // increase to the next row
+        return steps(n, row + 1);
+    }
+
+    // if the length of the 'stair' string is <= the row num we're working on, we add '#', otherwise we add a space
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';,
+    }
+
+    steps(n, row, stair)
+}
 
 module.exports = steps;
+
+// SOLUTION ONE: My function works but i can't pass the tests? Not sure why
+// function steps(n) {
+//     console.log(n);
+
+//     // Step 1: initialaize an array
+//     let stack = [];
+
+//     // Step 2: create a for loop that iterates up to 'n'
+//     for (let i = 0; i <= n - 1; i++) {
+//       // for each iteration, add cooresponding '#' 'i' times
+//       let pound = "#".repeat(i + 1);
+//       // console.log(pound);
+
+//       // add remaining spaces equal to 'i' - 1 so the length is = to 'n'
+//       let remainingSpaces = pound + "-".repeat(n - (i + 1));
+//       // console.log(remainingSpaces);
+
+//       // push values into stack array
+//       stack.push(remainingSpaces);
+
+//       // Step 3: join the array and use '\n' to set each value on its own line
+//       console.log(stack.join("\n"));
+//     }
+// }
+
+// SOLUTION TWO: console.logs out each stair, but I'm hesitant because it's quadratic time -> 0(n^2)
+// function steps(n) {
+//     for (let row = 0; row <= n - 1; row++) {
+//         let stair = '';
+
+//         for (let col = 0; col < n; col++ ) {
+//             if (col <= row) {
+//                 stair += '#'
+//             } else {
+//                 stair += " "
+//             }
+//         }
+//         // keep console.log within outer for loop
+//         // one console.log for each row we are attempting to build
+//         console.log(stair)
+//     }
+// }
+
+// SOLUTION THREE: recursion
+// function steps(n, row = 0, stair = '', ) {
+//     // Base Case
+//     // if (row === end), we've hit the end of our problem
+//     if (n === row) {
+//         return;
+//     }
+
+//     // if the 'stair' string has a length === n, then we are at the end of a row
+//     if (n === stair.length) {
+//         console.log(stair);
+//         // increase to the next row
+//         return steps(n, row + 1);
+//     }
+
+//     // if the length of the 'stair' string is <= the row num we're working on, we add '#', otherwise we add a space
+//     if (stair.length <= row) {
+//         stair += '#';
+//     } else {
+//         stair += ' ';
+//     }
+
+//     steps(n, row, stair)
+// }
